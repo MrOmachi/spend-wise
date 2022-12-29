3af4495931d816133d0d09b388b379e048430a0b
Omachi John <omachi.john@yahoo.com>
2022-12-28 20:46:36 -0800

    Style home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./components/styles/styles.css";
 import Home from "./pages/home/Home";
 import NavBar from "./pages/navBar/NavBar";
 import Footer from "./pages/footer/Footer";
@@ -7,9 +8,13 @@ import Footer from "./pages/footer/Footer";
 function App() {
   return (
     <>
-      <NavBar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="jumbo-tron">
+      <div className="jumbo-heading-text">
+        <div className="jumbo-new">
+          <Link className="jumbo-new-link">
+            <span className="span-new">NEW</span> <span className="span-mono">mono is live in kenya</span>{" "}
+            <AiOutlineArrowRight />
+          </Link>
+        </div>
+        <h1 className="jumbo-head">
+          Reliably access <br /> bank accounts
+        </h1>
+        <div className="jumpa-paragraph">
+          Mono helps businesses to access high-quality <br /> financial data and
+          direct bank payments.
+        </div>
+        <div className="jumbo-buttons">
+          <button className="button-one">
+            Get started for free <AiOutlineArrowRight />
+          </button>
+          <button className="button-two">
+            Contact Sales <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
+      <div className="jumbo-images"></div>
+    </div>
+  );
 }
 
 export default Home;
